ecaaa364a95bcf5821c80cf13024f266a05afb27
Colton-Grant <124389241+Colton-Grant@users.noreply.github.com>
2023-03-02 17:57:59 +0000

    added favicon.ico

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Colton
+// Created on: march 2023
 // This file contains the JS functions for index.html
+
+
+
+function myButtonClicked(){
+alert("Hello")
+}
+  
