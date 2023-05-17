e25e372f8c629ee2549e9f5aec8df75e54960018
Colton-Grant <124389241+Colton-Grant@users.noreply.github.com>
2023-05-17 17:05:29 +0000

    added sw.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,11 @@
 // Created on: march 2023
 // This file contains the JS functions for index.html
 
-
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-1-08-Colton-Grant/sw.js", {
+    scope: "/ICS2O-Unit-1-08-Colton-Grant/",
+  })
+}
 
 function myButtonClicked(){
 alert("Hello")
